17bde57553969a8880829bedc2db38edab1a6c15
Oziel Alves <ozielalves7@gmail.com>
2018-06-22 15:11:57 -0300

    SLPool

---
+++ include/SLPool.hpp
@@ -0,0 +1,108 @@
+/**
+ * @file SLPool.hpp
+ * @version 1.0
+ * @since Jun, 13. 
+ * @date Jun, 22.
+ * @author Oziel Alves (ozielalves@ufrn.edu.br)
+ * @author Daniel Guerra (daniel.guerra13@hotmail.com)
+ * @title Class sc::SLPool 
+ */
+
+/* "Para a implementação deste projeto é obrigatório a 
+	implementação da sua própria lista encadeada." 	*/
+
+#ifndef _SLPOOL_HPP_
+#define _SLPOOL_HPP_
+
+#include "storagepool.hpp"
+
+/**
+ * @brief The SLPool Class prototype
+ */
+
+namespace sc
+{
+	typedef unsigned int uint;
+	typedef T size_type;
+
+	template< typename T >
+	class SLPool : public StoragePool {
+ 
+	 public:
+    	/**
+    	 * @brief SLPool constructor
+    	 */
+    	explicit SLPool(size_type _b);
+
+    	/**
+    	 * @brief SLPool destructor
+    	 */
+    	~SLPool( );
+
+    	/**
+    	 * @brief Allocate memory
+    	 * @param _b Number of bytes to be allocated
+    	 * @return A pointer to the beggining of the allocated area
+    	 */
+    	void *Allocate(size_type _b);
+
+    	/**
+    	 * @brief Allocate memory using the Best Fit algorithm
+    	 * @param _b Number of bytes to be allocated
+    	 * @return A pointer to the beggining of the allocated area
+    	 */
+    	void *AllocateBF(size_type _b);
+
+    	/**
+    	 * @brief Free Memory
+   		 * @param _p A pointer to element to be freed
+    	 */
+    	void Free(void *_p);
+
+    	/**
+    	 * @brief Function to show a visual representation from memory Blocks
+    	 */
+    	void view( );
+
+    	/**
+    	 * @brief The header of the memory block
+    	 */
+    	struct Header {
+        	
+        	uint m_Length;  //!< The block's size
+        	
+        	//! Header Constructor
+        	Header( ) : m_Length(0u) { /*Empty*/ }
+    	};
+
+    	/**
+    	 * @brief The reserved memory (or a pointer to another block)
+    	 */
+    	struct Block : public Header {
+        	
+        	//! A enum with the memory block size
+        	enum {
+            
+            	BlockSize = 16  // Each block has 16 bytes.
+        	};
+
+        	//! A union with a pointer to the next block or the allocated memory
+        	union {
+            	
+            	Block *m_next;  //!< A pointer to the next block
+            	
+            	//! The allocated memory
+            	char m_raw[ BlockSize - sizeof(Header) ]; // Client's raw area
+        	};
+
+        	//! The Block constructor
+        	Block( ) : Header( ), mp_Next(nullptr) { /*Empty*/ };
+    	};
+
+	 private:
+    	uint mui_NumberOfBlocks;	//!< The number of blocks
+    	Block *m_pool;				//!< Head of list.
+    	Block &m_sentinel;			//!< End of the list.
+};
+
+#endif
--- include/storage_pool.hpp
+++ include/storage_pool.hpp
@@ -1,28 +1,53 @@
 /**
  * @file storage_pool.hpp
  * @version 1.0
- * @date May, 32.
- * @authors Daniel Guerra and Oziel Alves
+ * @since Jun, 13. 
+ * @date Jun, 22.
+ * @author Oziel Alves (ozielalves@ufrn.edu.br)
+ * @author Daniel Guerra (daniel.guerra13@hotmail.com)
  * @title Class sc::StoragePool
+ */
+
+/* "Para a implementação deste projeto é obrigatório a 
+	implementação da sua própria lista encadeada." 	*/
+
+# ifndef _STORAGE_POOL_HPP_
+# define _STORAGE_POOL_HPP_
+
+#include <iostream>
+//#include <vector>
+
+/**
  * @brief StoragePool class's declaration
  */
 
-# ifndef _STORAGE_POOL_
-# define _VECTOR_HPP_
-
-#include <iostream>
-#include <vector>
-
 namespace sc
 {
+	typedef T size_type;
+	
 	template< typename T >
-	class StoragePool
-	{
+	class StoragePool{
 		
-		private:
+	//private:
 
-		public:
+	public:
+    	/**
+    	 * @brief StoragePool destructor
+    	 */
+    	virtual ~StoragePool( ) { /*Empty*/ }
 
+    	/**
+    	 * @brief Allocates memory
+	     * @param _b Number of bytes to be allocated
+	     * @return A pointer to the beggining of the allocated area
+    	 */
+	    virtual void *Allocate( size_type _b ) /*= 0*/;
+
+	    /**
+	     * @brief Free memory
+	     * @param _p A pointer to element to be freed
+	     */
+	    virtual void Free(void *_p) /*= 0*/;
 	}
 
 }
